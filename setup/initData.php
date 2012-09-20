ddbc570d314ccd162cf56771fe30f8fea2c81e9e
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:18:53 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -2,17 +2,9 @@
 class workflow_Setup extends object_InitDataSetup
 {
 	public function install()
-	{	
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('workflow', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+	{
+		$this->executeModuleScript('init.xml');
+		
 		workflow_ModuleService::getInstance()->addExecuteScheduledTasks();
 	}
 }
